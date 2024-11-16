2bcecad2f1e31a56a5bd19e201f2b9ebe72b6396
yash <sharma2004yash@gmail.com>
2024-11-16 09:17:55 +0530

    changes

---
+++ client/src/App.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/shared/navbar.jsx'
+import { useParams,useLocation } from 'react-router-dom'
+import Privateroute from './components/auth/privateroute.jsx'
+import Form from './pages/UserForm.jsx'
+import Home from './pages/Home.jsx'
+import Auth from './pages/Auth.jsx'
+import Profile from './pages/Profile.jsx'
+import Meals from './pages/Meals.jsx'
+import Dite from './pages/Diet.jsx'
+import { useSelector } from 'react-redux'
+import MedicineForm from './pages/MedicineForm.jsx'
+import SideMenu from './components/shared/SideMenu.jsx'
+import Review from './pages/Review.jsx'
+
+// Get user id rom url using useParams
+
+function App() {
+  return (
+    <Router>
+              
+      <RoutesWithNavbar />
+    </Router>
+  );
+}
+  function RoutesWithNavbar() {
+    const location = useLocation();
+    const noNavbarRoutes = ['/auth', '/register']; 
+    const showNavbar = !noNavbarRoutes.includes(location.pathname);
+    const {isLogin} = useSelector((state)=>state.userInfo);
+    console.log(showNavbar);
+  return (
+    <>
+    <div className='flex overflow-auto'>
+    {showNavbar && <SideMenu />}
+    <Routes>
+          <Route path='/auth' element={<Privateroute user={!isLogin} path='/'>
+            <Auth />
+          </Privateroute>} />
+          <Route element={<Privateroute user={isLogin}/>}>
+            <Route path='/' element={<Home />} />
+            <Route path='/dashboard' element={<Home />} />
+            <Route path='/profile' element={<Profile />} />
+            <Route path='/setmedicine' element={<MedicineForm />} />
+            <Route path='/meals/:id' element={<Meals />} />
+            <Route path='/dite' element={<Dite />} />
+            <Route path='/review' element={<Review />} />
+          </Route> 
+          <Route element={<Privateroute user={!isLogin}/>}>
+            <Route path='/register' element={<Form />} />
+          </Route> 
+
+        </Routes>
+    </div>
+
+    </>
+  )
+}
+
+export default App
+
---
+++ client/src/pages/home.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import Camera from '../components/shared/Camera.jsx'
+import WeightChart from '../components/Charts/WeightChart.jsx'
+import BasicModal from '../components/shared/BasicModal.jsx'
+import CalorieChart from '../components/Charts/CalorieChart.jsx'
+import {getLast7DaysMeals} from '../util/methods'
+import axios from 'axios'
+function Home() {
+  // Home page
+  // Change tab title to 'Home'
+  const [userStatus, setUserStatus] = React.useState(null);
+  const [userMeals, setUserMeals] = React.useState(null);
+  console.log(import.meta.env.VITE_SERVER_URL);
+  React.useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get(`${import.meta.env.VITE_SERVER_URL}user/status`,
+          { withCredentials: true } // Important for sending cookies
+        );
+        setUserStatus(response.data);
+        const mealRespons = await axios.get(`${import.meta.env.VITE_SERVER_URL}calorie/getMeals`, { withCredentials: true });
+        console.log(mealRespons.data);
+        // console.log(response.data);
+        setUserMeals(mealRespons.data);
+      }
+      catch (e) {
+        console.log(e);
+      }
+    };
+
+    fetchData();
+  }, []);
+  const [modalStat, setModalStat] = React.useState({
+    open: false,
+    title: '',
+    description: ''
+  });
+  document.title = 'Home'
+  return (
+    <>
+      <BasicModal modalStat={modalStat} setOpen={setModalStat} />
+      <div className='h-screen w-4/5 bg-slate-50 overflow-auto '>
+        <div className=' h-full grid grid-rows-2 grid-cols-2 gap-2 p-2'>
+          <div className="p-2">
+          <div className=' flex flex-col justify-center items-start h-full px-10 gap-5 '>
+            <p className=' text-2xl rounded-xl p-2 font-semibold text-[#ffd60a] bg-gray-700'>
+              Track your daily Calorie int
+            </p>
+            <p className='text-5xl font-semibold text-gray-700'>
+              Track your Daily <span className='text-[#ffd60a]'>Calorie intake</span> with MyHealth
+            </p>
+          </div>
+          </div>
+          <div className=" flex  items-center">
+          {userStatus && <Camera userStatus={userStatus} triggerModal={setModalStat} />}
+          </div>
+          <div className=" p-4"><CalorieChart mealData={ userMeals && getLast7DaysMeals(userMeals)} userStatus={userStatus}  className="h-full w-full" /></div>
+          <div className=" p-4"><WeightChart className="h-full w-full" /></div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Home
